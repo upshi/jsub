cb4bc8cb0fe12b1f6c1c7a994982b0d8ad906404
upshi <827743366@qq.com>
2017-01-22 12:38:55 +0800

    first commit

---
+++ src/main/java/cn/upshi/App.java
@@ -0,0 +1,13 @@
+package cn.upshi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
